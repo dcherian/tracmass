305edcf8c32b74d75540f898eb5e2763a2ccd06c
dcherian <dcherian@mit.edu>
2013-06-20 13:02:36 -0400

    some mods to writetracer

--- src/writetracer.f95
+++ src/writetracer.f95
@@ -1,24 +1,24 @@
   ! write the simulated tracer
   subroutine writetracer
 #ifdef tracer
+
     USE mod_param
     USE mod_name
     USE mod_time
     USE mod_tracer
     USE mod_grid
+    USE mod_write
 
     IMPLICIT none
-    
-    CHARACTER(LEN=200)                         :: outDataDir, outDataFile
+   
     INTEGER nwx
-    print *,'outDataDir', outDataDir
-    nwx=IMT*JMT*KM*4
+ 
+    nwx=IMT*JMT*KM*8
     open(53,file=trim(outDataDir)//trim(outDataFile)//'_tracerfile',form='unformatted', &
          access='DIRECT',recl=nwx)
     write(53,rec=ints+1) tra
-close(53)
-return
+    close(53)
+    return
+
 #endif /*tracer*/
 end subroutine writetracer
-
-
